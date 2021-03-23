05ae291ac31bd570aa61fc7b4a49a8618fe1321f
Edward Montes Hernández <edward.montesh@gmail.com>
2021-03-22 21:45:35 -0600

    install

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+################################Install Docker################################
+sudo apt-get update -y
+sudo apt-get install -y\
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y 
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo usermod -aG docker ubuntu
+logout
+
+################################Install Minikube################################
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+
+################################Install Kubectl################################
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
